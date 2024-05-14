25921a1fae69f5d8372da64d2b92bc6cac22032e
Dylan Polito <dpolito1123@gmail.com>
2024-05-14 11:20:42 -0500

    started making the layout of the website. Added logo and header

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+require('dotenv').config();
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('src'))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src', 'index.html'))
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server connected at port ${process.env.PORT}`)
+})
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.html",
+    "./src/index.html"
+  ],
+  theme: {
+    fontFamily: {
+      'kaushan': ['Kaushan Script', 'cursive'],
+    },
+  },
+  plugins: [],
+}
