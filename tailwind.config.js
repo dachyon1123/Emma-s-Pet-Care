813138414899b1cd0562f61fa1c595820b0859b2
Dylan <dpolito1123@gmail.com>
2024-05-15 12:23:40 -0500

    changed the styling on the header. Added an about me section and a rates section to the front page. Currently working on getting a background image set on the first div.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,22 @@ module.exports = {
     "./src/index.html"
   ],
   theme: {
+   extend: {
     fontFamily: {
       'kaushan': ['Kaushan Script', 'cursive'],
+      'neucha': ["Neucha", 'cursive'],
+      'sriacha': ["Sriracha", 'cursive']
     },
+    colors: {
+      "primaryColor": '#859D82',
+      "secondaryColor": '#D15F0e',
+      "accentColor": '#f1eacf',
+      "white": "white"
+    },
+    backgroundImage: {
+      'sabo-image': "url('. /src/assets/sabo1.jpg')"
+    }
+   }
   },
   plugins: [],
 }
