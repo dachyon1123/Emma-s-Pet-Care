1a0b8c84b66705d53aa7f2fdc71d2d0bdf0360cf
Dylan Polito <dpolito1123@gmail.com>
2024-05-15 21:46:45 -0500

    Added and removed designs from front page. Added more pictures, and a table for the rates the client will charge their clients.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.html",
-    "./src/index.html"
-  ],
+  content: ["./src/**/*.html", "./src/index.html"],
   theme: {
-   extend: {
-    fontFamily: {
-      'kaushan': ['Kaushan Script', 'cursive'],
-      'neucha': ["Neucha", 'cursive'],
-      'sriacha': ["Sriracha", 'cursive']
+    extend: {
+      fontFamily: {
+        kaushan: ["Kaushan Script", "cursive"],
+        neucha: ["Neucha", "cursive"],
+        sriacha: ["Sriracha", "cursive"],
+        kalam: ["Kalam", 'cursive'],
+        contrail: ["Contrail-One", 'sans-serif'],
+        poetsen: ["Poetsen One", 'sans-serif'],
+        dosis: ["Dosis", 'sans-serif']
+      },
+      colors: {
+        primaryColor: "#859D82",
+        secondaryColor: "#D15F0e",
+        accentColor: "#f1eacf",
+        white: "white",
+      },
+      backgroundImage: {
+        "sabo-image": "url('./assets/kona1.jpg')",
+      },
     },
-    colors: {
-      "primaryColor": '#859D82',
-      "secondaryColor": '#D15F0e',
-      "accentColor": '#f1eacf',
-      "white": "white"
-    },
-    backgroundImage: {
-      'sabo-image': "url('. /src/assets/sabo1.jpg')"
-    }
-   }
   },
   plugins: [],
-}
+};
